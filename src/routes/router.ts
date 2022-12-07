ef7c0f8ac682895a4b347c16ea4619ee100b0c76
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-12-07 16:18:50 +0000

    fix(route): health route was not working

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,10 +1,23 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 
 import SocketGateway from "../socket/gateway";
-import health from "./health";
 
 export default async function router(fastify: FastifyInstance) {
   // Socket Gateway
   fastify.register(SocketGateway, { prefix: "/ws" });
-  fastify.register(health, { prefix: "/health" });
+
+  // Health Route
+  fastify.get("/health", (request: FastifyRequest, reply: FastifyReply) => {
+    reply.status(200).send({
+      statusCode: 200,
+      message: "Success",
+      payload: {
+        status: "healthy",
+        uptime: process.uptime(),
+        timestamp: Date.now(),
+        environment: process.env.NODE_ENV,
+        build_sha: process.env.COMMIT_SHA,
+      }
+    })
+  })
 }
