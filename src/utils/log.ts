8a5f369761ae431118c09f98493427d1b660e4f9
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-12-07 13:55:08 +0000

    feat: added basic stuff

---
+++ src/utils/log.ts
@@ -0,0 +1,11 @@
+import pino from "pino";
+import dayjs from "dayjs";
+
+const log = pino({
+    base: {
+        pid: false,
+    },
+    timestamp: () => `,"time":"${dayjs().format()}"`,
+});
+
+export default log;
