44ef8add9eddf900ca87de90293cb24386fcd30c
Aelpxy <aelpxy@protonmail.com>
2022-12-09 08:27:24 +0000

    fix: use normal async arrow functions instead

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,10 +1,11 @@
 import argon2 from "argon2";
 
-export default {
-  hash: async (plain: string) => {
-    return await argon2.hash(plain);
-  },
-  verify: async (plain: string, hash: string) => {
-    return await argon2.verify(hash, plain);
-  },
+const hash = async (plain: string) => {
+  return await argon2.hash(plain);
 };
+
+const verify = async (plain: string, hash: string) => {
+  return await argon2.verify(hash, plain);
+};
+
+export default { hash, verify };
