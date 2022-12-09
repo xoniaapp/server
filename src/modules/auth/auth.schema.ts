b046b0dad201d538a968f67777f6f637ae36cc39
Aelpxy <aelpxy@protonmail.com>
2022-12-09 08:22:06 +0000

    feat: added login schema

--- src/modules/auth/auth.schema.ts
+++ src/modules/auth/auth.schema.ts
@@ -1,10 +1,15 @@
 import { z } from "zod";
 
 const SINGUP_SCHEMA = z.object({
-  username: z.string().max(32).describe("Some description for username"),
+  username: z.string().max(32),
   suffix: z.number().max(4).min(4),
   email: z.string().email().trim(),
   password: z.string().max(32).min(8),
 });
 
-export default { SINGUP_SCHEMA };
+const LOGIN_SCHEMA = z.object({
+  email: z.string().max(32),
+  password: z.string().max(32).min(8)
+})
+
+export default { SINGUP_SCHEMA, LOGIN_SCHEMA };
