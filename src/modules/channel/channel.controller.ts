011ac2a506f903a2877c0c268e5e9f1ab6a39d42
Aelpxy <aelpxy@protonmail.com>
2022-12-18 16:15:34 +0000

    feat: add channel folder

---
+++ src/modules/channel/channel.controller.ts
@@ -0,0 +1,15 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+
+const createChannel = async (request: FastifyRequest, reply: FastifyReply): Promise<void> => {
+}
+
+const deleteChannel = async (request: FastifyRequest, reply: FastifyReply): Promise<void> => {
+}
+
+const getChannel = async (request: FastifyRequest, reply: FastifyReply): Promise<void> => {
+}
+
+const getChannels = async (request: FastifyRequest, reply: FastifyReply): Promise<void> => {
+}
+
+export { createChannel, deleteChannel, getChannel, getChannels }
