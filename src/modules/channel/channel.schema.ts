442952d30655960c76e2e63a17f87ebda789b1c0
Aelpxy <aelpxy@protonmail.com>
2022-12-19 16:52:46 +0000

    feat: added skeleton schema

---
+++ src/modules/channel/channel.schema.ts
@@ -0,0 +1,19 @@
+import { createYupSchema } from "fastify-yup-schema";
+
+const CREATE_CHANNEL_SCHEMA = createYupSchema((yup) => ({
+    body: {},
+}));
+
+const DELETE_CHANNEL_SCHEMA = createYupSchema((yup) => ({
+    body: {},
+}));
+
+const UPDATE_CHANNEL_SCHEMA = createYupSchema((yup) => ({
+    body: {},
+}));
+
+const GET_SCHEMA = createYupSchema((yup) => ({
+    body: {},
+}));
+
+export { CREATE_CHANNEL_SCHEMA, DELETE_CHANNEL_SCHEMA, UPDATE_CHANNEL_SCHEMA, GET_SCHEMA };
