963f411d1d58463f70f64230e6cc2611bc4a9203
Aelpxy <aelpxy@protonmail.com>
2022-12-20 10:33:17 +0000

    feat: finished message events for socket.service

--- src/socket/socket.service.ts
+++ src/socket/socket.service.ts
@@ -1,8 +1,50 @@
 import db from "../utils/db";
 
 export default {
-  send_message: () => {},
-  delete_message: () => {},
-  edit_message: () => {},
-  get_message: () => {},
+  get_messages: async (channelId: string) => {
+    return await db.message.findMany({
+      where: {
+        channelId: channelId,
+      },
+    })
+  },
+  get_message: async (id: string) => {
+    return await db.message.findFirst({
+      where: {
+        id: id,
+      },
+    });
+  },
+  send_message: async (body: any) => {
+    return await db.message.create({
+      data: {
+        text: body.text,
+        attachment: body.attachment,
+        reaction: body.reaction,
+        authorId: body.authorId,
+        channelId: body.channelId,
+        guildId: body.guildId,
+        groupId: body.groupId,
+      },
+    });
+  },
+  edit_message: async (body: any, id: string) => {
+    return await db.message.update({
+      where: {
+        id: id,
+      },
+      data: {
+        text: body.text,
+        attachment: body.attachment,
+        reaction: body.reaction,
+      },
+    });
+  },
+  delete_message: async (id: string) => {
+    return await db.message.delete({
+      where: {
+        id: id,
+      }
+    })
+  },
 };
