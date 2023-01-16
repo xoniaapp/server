c87a4e90ecbeab9cd038d33f048918939e63f2a9
Aelpxy <aelpxy@protonmail.com>
2023-01-16 08:37:48 +0000

    rewrite everything

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,15 +1,10 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 
 import SocketGateway from "../socket/socket.gateway";
-import authRoute from "../modules/auth/auth.route";
-import channelRoute from "../modules/channel/channel.route";
 
 export default async function router(fastify: FastifyInstance) {
   fastify.register(SocketGateway, { prefix: "/ws" });
-
-  fastify.register(authRoute, { prefix: "/auth" });
-  fastify.register(channelRoute, { prefix: "/channel" });
-
+  
   fastify.get("/health", (request: FastifyRequest, reply: FastifyReply) => {
     reply.status(200).send({
       statusCode: 200,
--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -1,18 +1,10 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 
-import {
-  get_message,
-  get_messages,
-  send_message,
-  edit_message,
-  delete_message,
-} from "./socket.service";
-
 const SocketGateway = async (fastify: FastifyInstance) => {
   fastify.route({
     method: "GET",
     handler: (req: FastifyRequest, reply: FastifyReply) => {
-      reply.status(500).send({error: "Requires a websocket connection"});
+      reply.status(500).send({ error: "Requires a websocket connection" });
     },
     url: "/",
     wsHandler: (connection, req) => {
@@ -23,7 +15,7 @@ const SocketGateway = async (fastify: FastifyInstance) => {
 
       try {
         connection.socket.send(
-            JSON.stringify({type: "WebSocket", connected: true})
+          JSON.stringify({ type: "WebSocket", connected: true })
         );
 
         connection.socket.on("message", async (data: any) => {
@@ -31,53 +23,22 @@ const SocketGateway = async (fastify: FastifyInstance) => {
 
           if (payload.type) {
             if (payload.type === "get_messages") {
-              const messages = await get_messages(payload.data.channelId);
-              connection.socket.send(JSON.stringify(messages));
-
               return;
             }
 
             if (payload.type === "get_message") {
-              const message = await get_message(payload.data.messageId);
-              connection.socket.send(JSON.stringify(message));
-
               return;
             }
 
             if (payload.type === "send_message") {
-              const message = await send_message({
-                text: payload.data.text,
-                attachment: payload.data.attachment,
-                reaction: payload.data.reaction,
-                authorId: payload.data.authorId,
-                channelId: payload.data.channelId,
-                guildId: payload.data.guildId,
-                groupId: payload.data.groupId,
-              });
-
-              connection.socket.send(JSON.stringify(payload.data));
               return;
             }
 
             if (payload.type === "delete_message") {
-              const message = await delete_message(payload.data.id);
-
-              connection.socket.send(JSON.stringify(message));
-
               return;
             }
 
             if (payload.type === "edit_message") {
-              const message = await edit_message(
-                  {
-                    text: payload.data.text,
-                    attachment: payload.data.attachment,
-                    reaction: payload.data.reaction,
-                  },
-                  payload.data.id
-              );
-
-              connection.socket.send(JSON.stringify(message));
               return;
             }
 
@@ -85,18 +46,18 @@ const SocketGateway = async (fastify: FastifyInstance) => {
           }
 
           connection.socket.send(
-              JSON.stringify({error: "Event type was not included"})
+            JSON.stringify({ error: "Event type was not included" })
           );
 
           return;
         });
 
         connection.socket.on("close", () => {
-          connection.socket.send(JSON.stringify({terminated: true}));
+          connection.socket.send(JSON.stringify({ terminated: true }));
         });
       } catch (error) {
         connection.socket.on("close", () => {
-          connection.socket.send(JSON.stringify({error: true}));
+          connection.socket.send(JSON.stringify({ error: true }));
         });
       }
     },
