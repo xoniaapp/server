85e23824923875c7f7cba0281526424365a42d30
Aelpxy <aelpxy@protonmail.com>
2023-01-17 15:14:27 +0000

    style: format code

--- src/utils/id.ts
+++ src/utils/id.ts
@@ -2,9 +2,7 @@ import { v1, v4 } from "uuid";
 
 export class Generate {
   discriminator() {
-    return (Math.floor(Math.random() * 10000) + 10000)
-      .toString()
-      .substring(1);
+    return (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
   }
   snowflake() {
     return `${Date.now()}${Math.floor(100000 + Math.random() * 900000)}`;
