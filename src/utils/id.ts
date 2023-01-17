32ba12d1279716b9985f207577b304e48dbdb073
Aelpxy <aelpxy@protonmail.com>
2023-01-17 15:14:12 +0000

    feat: added discriminator func to Generate class

--- src/utils/id.ts
+++ src/utils/id.ts
@@ -1,6 +1,11 @@
 import { v1, v4 } from "uuid";
 
 export class Generate {
+  discriminator() {
+    return (Math.floor(Math.random() * 10000) + 10000)
+      .toString()
+      .substring(1);
+  }
   snowflake() {
     return `${Date.now()}${Math.floor(100000 + Math.random() * 900000)}`;
   }
