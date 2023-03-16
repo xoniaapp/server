9106b5e5fee87143dbe3456fb849d15e17f72701
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2023-03-16 16:48:08 +0600

    added `PORT` and `HASH` variables (unused)

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,7 @@
 const AVATAR_CONTENT_URL: string = `https://api.dicebear.com/`;
 
+// Later export them
+const SERVER_PORT: number = 8080;
+const RANDOM_HASH: string = `YOUR_HASH`;
+
 export { AVATAR_CONTENT_URL };
